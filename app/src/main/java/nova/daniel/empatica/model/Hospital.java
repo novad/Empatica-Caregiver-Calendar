0b5285926e17d244d35f98fe3e1fe13efe2186dd
Daniel <danielhnova@gmail.com>
2019-08-16 00:53:01 +0200

    Edited Hospital.java, added docstrings and refactored method and attributes names

--- app/src/main/java/nova/daniel/empatica/model/Hospital.java
+++ app/src/main/java/nova/daniel/empatica/model/Hospital.java
@@ -3,71 +3,84 @@ package nova.daniel.empatica.model;
 import android.content.Context;
 
 import androidx.fragment.app.FragmentActivity;
-import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
-import nova.daniel.empatica.ui.MainActivity;
 import nova.daniel.empatica.viewmodel.AppointmentViewModel;
 
+/**
+ * Represents the hospital, with a set of appointments for a specific date.
+ */
 public class Hospital {
-
-    private ArrayList<HourSlotModel> mHourSlotModelArrayList;
-    private Date mCurrentDate;
-
     private Context mContext;
+    private List<HourSlotModel> mHourSlotModelList; // Appointments by hour for the given date in mCurrentDate
+    private Date mCurrentDate; // date of the model
 
-    AppointmentViewModel mAppointmentViewModel;
+    private AppointmentViewModel mAppointmentViewModel; // Appointments ViewModel
 
-    OnUpdateListener listener;
+    private OnUpdateListener modelUpdateListener;
 
-    public Hospital(Context context, Date date, OnUpdateListener listener){
+    public Hospital(Context context, Date date, OnUpdateListener modelUpdateListener) {
         mContext = context;
         mCurrentDate = date;
-        this.listener = listener;
-
+        this.modelUpdateListener = modelUpdateListener;
         mAppointmentViewModel = ViewModelProviders.of((FragmentActivity) mContext).get(AppointmentViewModel.class);
-        mAppointmentViewModel.getAppointmentsForDate(date).observe((FragmentActivity) mContext, new Observer<List<Appointment>>() {
-            @Override
-            public void onChanged(List<Appointment> appointments) {
-                System.out.println("hewwo");
-                initializeSlots(appointments);
-                listener.updateAdapter(mHourSlotModelArrayList);
+    }
 
-            }
+    /**
+     * Updates the mAppointmentViewModel for the given date, fetching the respective appointments form the database.
+     *
+     * @param newDate New date of the model.
+     */
+    public void updateModelDate(Date newDate) {
+        this.mCurrentDate = newDate;
+        mAppointmentViewModel.getAppointmentsForDate(mCurrentDate).observe((FragmentActivity) mContext, appointments -> {
+            initializeSlots(appointments);
+            modelUpdateListener.updateAdapter(mHourSlotModelList);
         });
-
     }
 
-    public ArrayList<HourSlotModel> getHourSlotModelArrayList() {
-        return mHourSlotModelArrayList;
+    public List<HourSlotModel> getHourSlotModelArrayList() {
+        return mHourSlotModelList;
     }
 
-    public void initializeSlots(List<Appointment> appointments){
-        mHourSlotModelArrayList = new ArrayList<>();
-        //for loop for sections
+    /**
+     * Initialize each HourSlot model for each hour of the day
+     * @param appointments
+     */
+    public void initializeSlots(List<Appointment> appointments) {
+        mHourSlotModelList = new ArrayList<>();
         for (int i = 0; i <= 23; i++) {
-            mHourSlotModelArrayList.add(new HourSlotModel(
-                    i + ":00", mCurrentDate,
-                    getAppointmentsByHour(appointments, i)));
+            mHourSlotModelList.add(new HourSlotModel(i + ":00", getAppointmentsByHour(appointments, i)));
         }
     }
 
-    public List<Appointment> getAppointmentsByHour(List<Appointment> appointments, int hour){
+    /**
+     * Appends all appointments for the given hour to a list and return it. In case no appointments
+     * for that hour exist, then an empty non-null List is returned.
+     * see method initializeSlots.
+     *
+     * @param appointments Appointments
+     * @param hour         Hour of the day
+     * @return List of appointments for the given hour.
+     */
+    private List<Appointment> getAppointmentsByHour(List<Appointment> appointments, int hour){
         List<Appointment> hourAppointments = new ArrayList<>();
         if (appointments!=null)
             for(Appointment appointment : appointments) {
                 if(appointment.getHour() == hour)
                     hourAppointments.add(appointment);
             }
-
         return hourAppointments;
     }
 
-    public interface OnUpdateListener{
-        public void updateAdapter(List<HourSlotModel> model);
+    /**
+     * Interface to respond to updates in the list of {@link HourSlotModel}
+     */
+    public interface OnUpdateListener {
+        void updateAdapter(List<HourSlotModel> model);
     }
 }
