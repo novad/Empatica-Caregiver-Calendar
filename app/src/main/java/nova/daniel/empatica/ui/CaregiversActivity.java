a4ba8a911a916f7e41cb5851faeb85ce7c535035
Daniel <danielhnova@gmail.com>
2019-08-16 01:23:41 +0200

    Edited CaregiversActivity.java. Added javadocs and comments

--- app/src/main/java/nova/daniel/empatica/ui/CaregiversActivity.java
+++ app/src/main/java/nova/daniel/empatica/ui/CaregiversActivity.java
@@ -7,26 +7,30 @@ import android.view.View;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
-
-import java.util.List;
 
 import nova.daniel.empatica.R;
 import nova.daniel.empatica.adapter.CaregiverAdapter;
 import nova.daniel.empatica.model.Caregiver;
 import nova.daniel.empatica.viewmodel.CaregiverViewModel;
 
+/**
+ * Activity that displays the list of caregivers.
+ * The list is populated by using the {@link CaregiverViewModel} instance.
+ * A caregiver can be tapped to be selected, and its respective ID is returned in the result intent.
+ */
+@SuppressWarnings("deprecation")
 public class CaregiversActivity extends AppCompatActivity implements CaregiverAdapter.onItemClickListener {
 
-    public static final String SELECTED_CAREGIVER = "SELECTEDCAREGIVER";
+    // Intent names
+    public static final String SELECTED_CAREGIVER = "SELECTED_CAREGIVER";
 
     public RecyclerView mRecyclerView;
-    public CaregiverAdapter mAdapter;
+    public CaregiverAdapter mAdapter; // mRecyclerView adapter
 
-    private int mPage = 1;
+    private int mPage = 1; // Page of caregivers to fetch
 
     ProgressDialog progressDialog;
 
@@ -37,45 +41,64 @@ public class CaregiversActivity extends AppCompatActivity implements CaregiverAd
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_caregivers);
 
+        // Show loading dialog
         progressDialog = new ProgressDialog(CaregiversActivity.this);
         progressDialog.setMessage("Loading....");
         progressDialog.show();
 
+        // Set up recycler view
         mRecyclerView = findViewById(R.id.caregivers_recyclerView);
         mAdapter = new CaregiverAdapter(this);
         mRecyclerView.setAdapter(mAdapter);
         mRecyclerView.setLayoutManager(new LinearLayoutManager(getApplication()));
 
+        // Created ViewModel and update adapter in case of changes.
         mCaregiverViewModel = ViewModelProviders.of(this).get(CaregiverViewModel.class);
-        mCaregiverViewModel.getAllCaregivers().observe(this, new Observer<List<Caregiver>>() {
-            @Override
-            public void onChanged(List<Caregiver> caregiverEntities) {
-                if(caregiverEntities.size() != 0)
-                    progressDialog.dismiss();
-                mAdapter.setCaregiversList(caregiverEntities);
-            }
+        mCaregiverViewModel.getAllCaregivers().observe(this, caregiverEntities -> {
+            if (caregiverEntities.size() != 0)
+                progressDialog.dismiss();
+            mAdapter.setCaregiversList(caregiverEntities);
         });
     }
 
-    public void loadMoreOnClick(View view){
+    /**
+     * Call for more caregivers to be fetched from the view model depending on the current page of fetched results.
+     * A maximum of 100 caregivers can be fetched, if over that limit, an error Toast is displayed,
+     * and disable the "load more" button.
+     *
+     * @param view Clicked view
+     */
+    public void loadMoreOnClick(View view) {
+        // Check for fetch limit and disable the "load more" button
         if (mCaregiverViewModel.isOverLimit()){
             Toast.makeText(this, "Maximum allowed of fetched caregivers reached", Toast.LENGTH_SHORT).show();
             View button = findViewById(R.id.loadmore_button);
             button.setEnabled(false);
-        }else {
+        } else {
             progressDialog.show();
             mPage += 1;
             mCaregiverViewModel.fetchMoreCaregivers(mPage);
         }
     }
 
+    /**
+     * Calls the adapter to sort the list of caregivers.
+     * @param view Clicked View
+     */
     public void sortOnClick(View view){
         Toast.makeText(this, "Sorting by last name", Toast.LENGTH_SHORT).show();
         mAdapter.doSort();
     }
 
+
+    /**
+     * Implementation of the listener when a single Caregiver is clicked.
+     * Sends a result intent containing the selected caregiver's ID and then finishes the activity.
+     * @param caregiver Chosen caregiver
+     * @param position Position of the selected item
+     */
     @Override
-    public void onItemClick(Caregiver caregiver, int position) {
+    public void onCaregiverClick(Caregiver caregiver, int position) {
         Intent intent = new Intent();
         intent.putExtra(CaregiversActivity.SELECTED_CAREGIVER, caregiver.uuid);
         setResult(RESULT_OK, intent);
