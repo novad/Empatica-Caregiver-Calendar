1bafd432756798a1fbc0bfb927e7e673d0242495
Daniel <danielhnova@gmail.com>
2019-08-16 01:07:00 +0200

    Moved DAOs to the dao package

--- app/src/main/java/nova/daniel/empatica/persistence/dao/AppointmentDAO.java
+++ app/src/main/java/nova/daniel/empatica/persistence/dao/AppointmentDAO.java
@@ -1,4 +1,4 @@
-package nova.daniel.empatica.persistence;
+package nova.daniel.empatica.persistence.dao;
 
 import androidx.lifecycle.LiveData;
 import androidx.room.Dao;
@@ -6,11 +6,15 @@ import androidx.room.Delete;
 import androidx.room.Insert;
 import androidx.room.OnConflictStrategy;
 import androidx.room.Query;
+import androidx.room.Update;
 
 import java.util.List;
 
 import nova.daniel.empatica.model.Appointment;
 
+/**
+ * Direct access object for {@link Appointment} entities
+ */
 @Dao
 public interface AppointmentDAO {
 
@@ -18,20 +22,21 @@ public interface AppointmentDAO {
     LiveData<List<Appointment>> getAll();
 
     @Query("SELECT * FROM Appointment WHERE appointmentId IN (:appointmentIds)")
-    LiveData<List<Appointment>> loadAllByIds(int[] appointmentIds);
+    LiveData<List<Appointment>> getAllAllByIds(int[] appointmentIds);
 
     @Query("SELECT * FROM Appointment WHERE date BETWEEN :start AND :end")
     LiveData<List<Appointment>> getAppointmentsByDate(long start, long end);
 
-//
-//    @Query("SELECT * FROM user WHERE first_name LIKE :first AND " +
-//            "last_name LIKE :last LIMIT 1")
-//    AppointmentEntity findByName(String first, String last);
-
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     void insertAll(Appointment... appointments);
 
+    @Update
+    void update(Appointment... appointments);
+
     @Delete
-    void delete(Appointment appointment);
+    void delete(Appointment... appointment);
+
+    @Query("DELETE FROM Appointment WHERE appointmentId = :id")
+    void deleteById(int id);
 
 }
--- app/src/main/java/nova/daniel/empatica/persistence/dao/CaregiverDAO.java
+++ app/src/main/java/nova/daniel/empatica/persistence/dao/CaregiverDAO.java
@@ -1,8 +1,7 @@
-package nova.daniel.empatica.persistence;
+package nova.daniel.empatica.persistence.dao;
 
 import androidx.lifecycle.LiveData;
 import androidx.room.Dao;
-import androidx.room.Delete;
 import androidx.room.Insert;
 import androidx.room.OnConflictStrategy;
 import androidx.room.Query;
@@ -12,6 +11,9 @@ import java.util.List;
 
 import nova.daniel.empatica.model.Caregiver;
 
+/**
+ * Direct access object for {@link Caregiver} entities
+ */
 @Dao
 public interface CaregiverDAO {
 
@@ -20,19 +22,12 @@ public interface CaregiverDAO {
 
     @Query("SELECT * FROM Caregiver WHERE uuid IN (:caregiverIds)")
     LiveData<List<Caregiver>> loadAllByIds(String[] caregiverIds);
-//
-//    @Query("SELECT * FROM user WHERE first_name LIKE :first AND " +
-//            "last_name LIKE :last LIMIT 1")
-//    AppointmentEntity findByName(String first, String last);
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     long[] insertCaregivers(Caregiver... caregiverEntities);
 
     @Update
     void updateCaregivers(Caregiver... caregiverEntities);
-
-    @Delete
-    void delete(Caregiver caregiver);
 
     @Query("DELETE FROM Caregiver")
     void deleteAll();
