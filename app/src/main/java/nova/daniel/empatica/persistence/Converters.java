0fc14298eedf63de5068a233c7f3fba8c74eeabd
Daniel <danielhnova@gmail.com>
2019-08-16 01:51:26 +0200

    Type converters must be public

--- app/src/main/java/nova/daniel/empatica/persistence/Converters.java
+++ app/src/main/java/nova/daniel/empatica/persistence/Converters.java
@@ -7,14 +7,14 @@ import java.util.Date;
 /**
  * Converter class to save Date attributes as longs in the database
  */
-class Converters {
+public class Converters {
     @TypeConverter
-    static Date fromTimestamp(Long value) {
+    public static Date fromTimestamp(Long value) {
         return value == null ? null : new Date(value);
     }
 
     @TypeConverter
-    static Long dateToTimestamp(Date date) {
+    public static Long dateToTimestamp(Date date) {
         return date == null ? null : date.getTime();
     }
 }
