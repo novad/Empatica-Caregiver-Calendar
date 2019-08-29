e9167b8314757f3a333ae56104513f4f9124cf8e
Daniel <danielhnova@gmail.com>
2019-08-30 01:28:23 +0200

    Created CountWork

---
+++ app/src/main/java/nova/daniel/empatica/model/CountWork.java
@@ -0,0 +1,10 @@
+package nova.daniel.empatica.model;
+
+/**
+ * POJO class that represents the number of appointments for a caregiver's uuid.
+ * Used when fetching results from the repository.
+ */
+public class CountWork {
+    public String uuid;
+    public Integer counts;
+}
