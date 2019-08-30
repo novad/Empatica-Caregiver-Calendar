1e6119f127ab4616eddd3822af7a24f3a878f217
Daniel <danielhnova@gmail.com>
2019-08-30 04:23:03 +0200

    Added queries to fetch caregivers associated with appointments

--- app/src/main/java/nova/daniel/empatica/persistence/dao/AppointmentDAO.java
+++ app/src/main/java/nova/daniel/empatica/persistence/dao/AppointmentDAO.java
@@ -18,23 +18,6 @@ import nova.daniel.empatica.model.Appointment;
 @Dao
 public interface AppointmentDAO {
 
-    @Query("SELECT * FROM Appointment")
-    LiveData<List<Appointment>> getAll();
-
-    @Query("SELECT * FROM Appointment WHERE appointmentId IN (:appointmentIds)")
-    LiveData<List<Appointment>> getAllAllByIds(int[] appointmentIds);
-
-    @Query("SELECT * FROM Appointment WHERE date BETWEEN :start AND :end")
-    LiveData<List<Appointment>> getAppointmentsByDate(long start, long end);
-
-    @Query("SELECT room_number FROM Appointment WHERE date BETWEEN :start AND :end")
-    LiveData<List<Integer>> getAppointmentRoomsByDate(long start, long end);
-
-    @Query("SELECT uuid FROM Appointment WHERE " +
-            "appointmentId NOT IN (:appointmentIds) " +
-            "AND  date BETWEEN :start AND :end")
-    LiveData<List<String>> getCaregiversByDate(long start, long end, int[] appointmentIds);
-
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     void insertAll(Appointment... appointments);
 
@@ -44,9 +27,53 @@ public interface AppointmentDAO {
     @Delete
     void delete(Appointment... appointment);
 
+    @Query("DELETE FROM Appointment")
+    void deleteAll();
+
     @Query("DELETE FROM Appointment WHERE appointmentId = :id")
     void deleteById(int id);
 
+    @Query("SELECT * FROM Appointment")
+    LiveData<List<Appointment>> getAll();
+
+    @Query("SELECT * FROM Appointment WHERE appointmentId IN (:appointmentIds)")
+    LiveData<List<Appointment>> getAllByIds(int[] appointmentIds);
+
+    @Query("SELECT * FROM Appointment WHERE date BETWEEN :start AND :end ORDER BY room_number")
+    LiveData<List<Appointment>> getByDate(long start, long end);
+
+    @Query("SELECT room_number FROM Appointment WHERE date BETWEEN :start AND :end")
+    LiveData<List<Integer>> getRoomsByDate(long start, long end);
+
+    @Query("SELECT room_number FROM Appointment WHERE date BETWEEN :start AND :end")
+    List<Integer> getRoomsByDateSync(long start, long end);
+
+    @Query("SELECT room_number FROM Appointment WHERE uuid=:caregiverId AND date BETWEEN :start AND :end")
+    List<Integer> getRoomsForCaregiverByDate(long start, long end, String caregiverId);
+
+    //    Get caregivers for appointments in a given date range, with the option to exclude certain ids
+    @Query("SELECT uuid FROM Appointment WHERE " +
+            "appointmentId NOT IN (:appointmentIds) " +
+            "AND  date BETWEEN :start AND :end")
+    LiveData<List<String>> getCaregiversByDate(long start, long end, int[] appointmentIds);
+
+    @Query("SELECT uuid FROM Appointment " +
+            "WHERE date BETWEEN :start AND :end")
+    List<String> getCaregiversByDateSync(long start, long end);
+
     @Query("SELECT COUNT(*) FROM Appointment WHERE uuid=:caregiverId AND date BETWEEN :start AND :end")
-    LiveData<Integer> countCaregiverSlotsForWeek(long start, long end, String caregiverId);
+    LiveData<Integer> countByCaregiverForDate(long start, long end, String caregiverId);
+
+    @Query("SELECT COUNT(*) FROM Appointment WHERE uuid=:caregiverId AND date BETWEEN :start AND :end")
+    int countByCaregiverForDateSync(long start, long end, String caregiverId);
+
+    @Query("SELECT MIN(counts) " +
+            "FROM(" +
+            "SELECT uuid, COUNT(uuid) as counts " +
+            "FROM Appointment " +
+            "WHERE date BETWEEN :start AND :end " +
+            "GROUP BY uuid " +
+            "ORDER BY COUNT(*))")
+    int getMinNumAppointments(long start, long end);
+
 }
