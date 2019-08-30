29ef5d6198c75cb459cfe6571a72c3152549d410
Daniel <danielhnova@gmail.com>
2019-08-30 04:36:23 +0200

    Added methods to fetch room information of appointments

--- app/src/main/java/nova/daniel/empatica/persistence/AppointmentRepository.java
+++ app/src/main/java/nova/daniel/empatica/persistence/AppointmentRepository.java
@@ -1,119 +1,47 @@
 package nova.daniel.empatica.persistence;
 
-import android.app.Application;
 import android.content.Context;
 import android.os.AsyncTask;
-import android.provider.CalendarContract;
 
 import androidx.lifecycle.LiveData;
 
-import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 
+import nova.daniel.empatica.Utils;
 import nova.daniel.empatica.model.Appointment;
 import nova.daniel.empatica.persistence.dao.AppointmentDAO;
 
 /**
- * Deals with local persistence calls for {@link Appointment} objects.
+ * Repository that deals with local persistence calls for {@link Appointment} objects.
  */
 public class AppointmentRepository {
 
-    private Context mContext;
     private AppointmentDAO mAppointmentDAO;
     private LiveData<List<Appointment>> mAppointments;
 
-    public AppointmentRepository(Application application){
-        mContext = application.getApplicationContext();
-        AppDatabase db = AppDatabase.getInMemoryDatabase(application);
+    public AppointmentRepository(Context context) {
+        AppDatabase db = AppDatabase.getInMemoryDatabase(context);
         mAppointmentDAO = db.appointmentDAO();
     }
 
-    public LiveData<List<Appointment>> getAllAppointments(){
+    public LiveData<List<Appointment>> getAll() {
         mAppointments = mAppointmentDAO.getAll();
         return mAppointments;
     }
 
-    public LiveData<List<Appointment>> getAppointmentsById(int[] ids) {
-        mAppointments = mAppointmentDAO.getAllAllByIds(ids);
+    public LiveData<List<Appointment>> getByID(int[] ids) {
+        mAppointments = mAppointmentDAO.getAllByIds(ids);
         return mAppointments;
     }
-
-    /**
-     * Gets all appointments for a given day (between 0:00 - 23:59).
-     * @param date Date of appointments to fetch.
-     * @return List of appointments for the given date.
-     */
-    public LiveData<List<Appointment>> getAppointmentsForDate(Date date) {
-        Calendar cal = Calendar.getInstance();
-        cal.setTime(date);
-        cal.set(Calendar.HOUR_OF_DAY, 0);
-        cal.set(Calendar.MINUTE, 0);
-        long start = cal.getTime().getTime();
-
-        cal.set(Calendar.HOUR_OF_DAY, 23);
-        cal.set(Calendar.MINUTE, 59);
-        long end = cal.getTime().getTime();
-
-        mAppointments = mAppointmentDAO.getAppointmentsByDate(start, end);
-        return mAppointments;
-    }
-
-
-    /**
-     * Gets room numbers of appointments scheduled for a specific hour of the day
-     * @param date Date of the appointments to query
-     * @return List of rooms for the given date
-     */
-    public LiveData<List<Integer>> getUsedRoomsForDateHour(Date date) {
-        Calendar cal = Calendar.getInstance();
-        cal.setTime(date);
-        cal.set(Calendar.MINUTE, 0);
-        long start = cal.getTime().getTime();
-
-        cal.set(Calendar.MINUTE, 59);
-        long end = cal.getTime().getTime();
-
-        return mAppointmentDAO.getAppointmentRoomsByDate(start, end);
-    }
-
-
-    /**
-     * Gets ids of caregivers associated with appointments scheduled for a specific hour of the day
-     * The given appointment id is excluded from the query.
-     *
-     * @param date Date of the appointments to query
-     * @param appointmentId Appointment ID to exclude.
-     * @return List of caregivers ids for the given date
-     */
-    public LiveData<List<String>> getCaregiversForDateHour(Date date, int appointmentId) {
-        Calendar cal = Calendar.getInstance();
-        cal.setTime(date);
-        cal.set(Calendar.MINUTE, 0);
-        long start = cal.getTime().getTime();
-
-        cal.set(Calendar.MINUTE, 59);
-        long end = cal.getTime().getTime();
-
-        return mAppointmentDAO.getCaregiversByDate(start, end, new int[]{appointmentId});
-    }
-
-
-    public LiveData<Integer> countCaregiverSlotsForWeek(Date date, String caregiverId) {
-        Calendar cal = Calendar.getInstance();
-        cal.setTime(date);
-        cal.set(Calendar.DAY_OF_WEEK, cal.getFirstDayOfWeek());
-        long start = cal.getTime().getTime();
-
-        cal.add(Calendar.WEEK_OF_YEAR, 1);
-        long end = cal.getTime().getTime();
-
-        return mAppointmentDAO.countCaregiverSlotsForWeek(start, end, caregiverId);
-    }
-
 
     public void insert(Appointment appointment) {
         new AppointmentRepository.insertAsyncTask(mAppointmentDAO).execute(appointment);
+    }
+
+    // Synchronous calls of insert
+    public void insertSync(Appointment appointment) {
+        mAppointmentDAO.insertAll(appointment);
     }
 
     public void update(Appointment appointment) {
@@ -129,7 +57,110 @@ public class AppointmentRepository {
     }
 
     /**
-     * Async task to insert appointments into the database
+     * Get all appointments from a given epoch time range
+     *
+     * @param start Start epoch time
+     * @param end   End epoch tome
+     * @return List of appointments in the given range
+     */
+    public LiveData<List<Appointment>> getForDate(long start, long end) {
+        mAppointments = mAppointmentDAO.getByDate(start, end);
+        return mAppointments;
+    }
+
+
+    /**
+     * Gets room numbers of appointments scheduled for a epoch time range
+     *
+     * @param start Start time to query
+     * @param end   End time to query
+     * @return List of rooms for the given date
+     */
+    public LiveData<List<Integer>> getUsedRoomsForTimeRange(long start, long end) {
+        return mAppointmentDAO.getRoomsByDate(start, end);
+    }
+
+    /**
+     * Gets ids of caregivers associated with appointments scheduled for a the given hour.
+     * The given appointment id is excluded from the query.
+     *
+     * @param date          Target date
+     * @param appointmentId Appointment ID to exclude.
+     * @return List of caregivers ids for the given date
+     */
+    public LiveData<List<String>> getCaregiversForHour(Date date, int appointmentId) {
+        long start = Utils.removeMinutesSecondsAndMillis(date).getTime(); // set minute to 0 for the current hour
+        long end = Utils.getHourEnd(date).getTime();
+
+        return mAppointmentDAO.getCaregiversByDate(start, end, new int[]{appointmentId});
+    }
+
+    /**
+     * Synchronous call for getCaregiversForHour. Gets ids of caregivers associated with appointments scheduled for a the given hour.
+     * The given appointment id is excluded from the query.
+     *
+     * @param date Target date
+     * @return List of caregivers ids for the given date
+     */
+    public List<String> getCaregiversForHourSync(Date date) {
+        long start = Utils.removeMinutesSecondsAndMillis(date).getTime(); // set minute to 0 for the current hour
+        long end = Utils.getHourEnd(date).getTime();
+
+        return mAppointmentDAO.getCaregiversByDateSync(start, end);
+    }
+
+    /**
+     * Counts how many appointments have been assigned to the given caregiver in during the week in the given date.
+     *
+     * @param caregiverId id of the caregiver
+     * @return Number of times the given caregiver is assigned to appointments in the given week.
+     */
+    public LiveData<Integer> countCaregiverSlotsForWeek(Date date, String caregiverId) {
+        Date start = Utils.getStartOfWeek(date);
+        Date end = Utils.advanceWeek(start);
+        return mAppointmentDAO.countByCaregiverForDate(start.getTime(), end.getTime(), caregiverId);
+    }
+
+    /**
+     * Sync impl of countCaregiverSlotsForWeek
+     * Counts how many appointments have been assigned to the given caregiver in during the week in the given date.
+     *
+     * @param caregiverId id of the caregiver
+     * @return Number of times the given caregiver is assigned to appointments in the given week.
+     */
+    public int countCaregiverSlotsForWeekSync(Date date, String caregiverId) {
+        Date start = Utils.getStartOfWeek(date);
+        Date end = Utils.advanceWeek(start);
+        return mAppointmentDAO.countByCaregiverForDateSync(start.getTime(), end.getTime(), caregiverId);
+    }
+
+    /**
+     * Synchronously gets the list of room numbers that have been assigned to a single caregiver in a given day.
+     *
+     * @param date        Target date
+     * @param caregiverId Caregiver to query
+     * @return List of room numbers
+     */
+    public List<Integer> getRoomsForCaregiverByDay(Date date, String caregiverId) {
+        Date start = Utils.getDayStart(date);
+        Date end = Utils.getDayEnd(date);
+        return mAppointmentDAO.getRoomsForCaregiverByDate(start.getTime(), end.getTime(), caregiverId);
+    }
+
+    /**
+     * Synchronously gets the list of room numbers that have been assigned in a given hour.
+     *
+     * @param date Target date
+     * @return List of room numbers
+     */
+    public List<Integer> getRoomsForCaregiverByHour(Date date) {
+        long start = Utils.removeMinutesSecondsAndMillis(date).getTime(); // set minute to 0 for the current hour
+        long end = Utils.getHourEnd(date).getTime();
+        return mAppointmentDAO.getRoomsByDateSync(start, end);
+    }
+
+    /**
+     * Async task to insert {@link Appointment} objects into the database
      */
     private static class insertAsyncTask extends AsyncTask<Appointment, Void, Void> {
         private AppointmentDAO mAsyncTaskDao;
@@ -146,7 +177,7 @@ public class AppointmentRepository {
     }
 
     /**
-     * Async task to update appointments in the database
+     * Async task to update {@link Appointment} objects in the database
      */
     private static class updateAsyncTask extends AsyncTask<Appointment, Void, Void> {
         private AppointmentDAO mAsyncTaskDao;
@@ -163,7 +194,7 @@ public class AppointmentRepository {
     }
 
     /**
-     * Async task to delete appointments from the database, either by object or ID, depending on the constructor used.
+     * Async task to delete {@link Appointment} objects from the database, either by object or ID, depending on the constructor used.
      */
     private static class deleteAsyncTask extends AsyncTask<Appointment, Void, Void> {
         private AppointmentDAO mAsyncTaskDao;
