3559f6058b0120eee0dacfc4f53472b53dc03ec3
Daniel <danielhnova@gmail.com>
2019-08-30 05:32:44 +0200

    Added javadocs

--- app/src/main/java/nova/daniel/empatica/viewmodel/CaregiverViewModel.java
+++ app/src/main/java/nova/daniel/empatica/viewmodel/CaregiverViewModel.java
@@ -12,35 +12,55 @@ import nova.daniel.empatica.R;
 import nova.daniel.empatica.model.Caregiver;
 import nova.daniel.empatica.persistence.CaregiverRepository;
 
+/**
+ * ViewModel for caregivers
+ */
 public class CaregiverViewModel extends AndroidViewModel {
 
     private CaregiverRepository mRepository;
-
     private LiveData<List<Caregiver>> mCaregivers;
 
     public CaregiverViewModel(@NonNull Application application) {
         super(application);
-        mRepository = new CaregiverRepository(application);
+        mRepository = new CaregiverRepository(application, true);
     }
 
-    public LiveData<List<Caregiver>> getAllCaregivers(){
-        mCaregivers = mRepository.getAllCaregivers();
+    public LiveData<List<Caregiver>> getAll() {
+        mCaregivers = mRepository.getAll();
         return mCaregivers;
     }
 
-    public LiveData<List<Caregiver>> getCaregiverByID(String[] caregiverIds){
-        mCaregivers =  mRepository.getCaregiverById(caregiverIds);
+    // Fetch all IDs
+    public LiveData<List<String>> getAllIDs() {
+        return mRepository.getAllIDs();
+    }
+
+    public LiveData<List<Caregiver>> getByID(String[] caregiverIds) {
+        mCaregivers = mRepository.getByID(caregiverIds);
         return mCaregivers;
     }
 
+    /**
+     * Calls the repository to fetch more caregivers. Initially fetches a page of 1.
+     *
+     * @param mPage Page to fetch.
+     */
     public void fetchMoreCaregivers(int mPage) {
-        mRepository.loadCaregivers(mPage);
+        mRepository.fetchCaregivers(mPage);
     }
 
-    public void add(Caregiver caregiver){mRepository.insert(caregiver);}
+    public void add(Caregiver caregiver) {
+        mRepository.insert(caregiver);
+    }
 
+    /**
+     * Checks if the current number of caregivers exceeds the maximum.
+     * This maximum is set in the integers resource file, under api_max_results, with a default value of 100.
+     *
+     * @return True if there are equal or more than api_max_results entries in the repository, false if the total number is less than api_max_results
+     */
     public boolean isOverLimit(){
         int max = getApplication().getResources().getInteger(R.integer.api_max_results);
-        return mCaregivers.getValue().size() >= max;
+        return mCaregivers.getValue() != null && mCaregivers.getValue().size() >= max;
     }
 }
